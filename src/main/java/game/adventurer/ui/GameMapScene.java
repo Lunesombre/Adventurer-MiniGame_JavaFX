c7c2cdcda763f57a94cc634b9cfd1b577119ac14
Romain Touchet <romain.touchet@gmail.com>
2024-09-01 23:49:17 +0200

    feat(GameMap improvements): externalized the scene dimensions configurations + factory on GameMapScene + map is now resized (and centered) to fit changes in window dimensions
    - externalized the scene dimensions configurations;
    - used singleton pattern on the SceneConfig class;
    - refactored GameMapScene to use a factory pattern;
    - map is now resized (and centered) to fit changes in window dimensions

--- src/main/java/game/adventurer/ui/GameMapScene.java
+++ src/main/java/game/adventurer/ui/GameMapScene.java
@@ -1,36 +1,105 @@
 package game.adventurer.ui;
 
+import game.adventurer.config.SceneConfig;
 import game.adventurer.model.GameMap;
 import game.adventurer.model.Tile;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 
 public class GameMapScene extends Scene {
 
-  public GameMapScene(GameMap gameMap) {
-    super(new GridPane(), 800, 600);
-    GridPane grid = (GridPane) getRoot();
+  private static final int PADDING = 20;
+  private GameMap gameMap;
+  private Pane gamePane;
 
-    // représentation visuelle de la grille
-    for (int y = 0; y < gameMap.getMapHeight(); y++) {
-      for (int x = 0; x < gameMap.getMapWidth(); x++) {
+
+  //V3 pattern Factory
+  private GameMapScene(StackPane root, int width, int height) {
+    super(root, width, height);
+  }
+
+  public static GameMapScene create(GameMap gameMap) {
+    SceneConfig config = SceneConfig.getInstance();
+    StackPane root = new StackPane();
+    GameMapScene scene = new GameMapScene(root, config.getWidth(), config.getHeight());
+    scene.gameMap = gameMap;
+    scene.initializeScene();
+    return scene;
+  }
+
+  private void initializeScene() {
+    SceneConfig config = SceneConfig.getInstance();
+    int windowWidth = config.getWidth();
+    int windowHeight = config.getHeight();
+
+    StackPane root = (StackPane) getRoot();
+    root.setPrefSize(windowWidth, windowHeight);
+
+    gamePane = new Pane();
+    root.getChildren().add(gamePane);
+
+    // Add a listeners for resizing
+    widthProperty().addListener((obs, oldVal, newVal) -> updateGameMap());
+    heightProperty().addListener((obs, oldVal, newVal) -> updateGameMap());
+
+    // Initialiser la carte
+    updateGameMap();
+  }
+
+  private void updateGameMap() {
+    gamePane.getChildren().clear();
+    double windowWidth = getWidth();
+    double windowHeight = getHeight();
+    int mapWidth = gameMap.getMapWidth();
+    int mapHeight = gameMap.getMapHeight();
+
+    // Calculate tile size to adapt to window
+    double tileSize = Math.min(
+        (windowWidth - 2 * PADDING) / mapWidth,
+        (windowHeight - 2 * PADDING) / mapHeight
+    );
+
+    // Calculate the offset to center the map
+    double xOffset = (windowWidth - mapWidth * tileSize) / 2;
+    double yOffset = (windowHeight - mapHeight * tileSize) / 2;
+
+    // Create and position tiles representation
+    for (int y = 0; y < mapHeight; y++) {
+      for (int x = 0; x < mapWidth; x++) {
         Tile tile = gameMap.getGrid()[y][x];
-        Rectangle rect = new Rectangle(30, 30);
+        Rectangle rect = new Rectangle(tileSize, tileSize);
         rect.setFill(tile.getType() == Tile.Type.PATH ? Color.web("#B87065") : Color.web("#206600"));
-        grid.add(rect, x, y);
+        rect.setX(xOffset + x * tileSize);
+        rect.setY(yOffset + y * tileSize);
+        rect.setStroke(Color.web("#4a5246")); // grey-greenish tile border
+        rect.setStrokeWidth(0.5);
+        gamePane.getChildren().add(rect);
       }
     }
 
-    // représentation de l'aventurier et du trésor
-    Circle adventurer = new Circle(15, Color.BLUE);
-    grid.add(adventurer, gameMap.getAdventurer().getX(), gameMap.getAdventurer().getY());
+    // Adding adventurer
+    Circle adventurer = new Circle(tileSize / 2, Color.BLUE);
+    adventurer.setCenterX(xOffset + (gameMap.getAdventurer().getX() + 0.5) * tileSize);
+    adventurer.setCenterY(yOffset + (gameMap.getAdventurer().getY() + 0.5) * tileSize);
+    gamePane.getChildren().add(adventurer);
 
-    Circle treasure = new Circle(15, Color.GOLD);
-    grid.add(treasure, gameMap.getTreasure().getX(), gameMap.getTreasure().getY());
+    // Adding treasure
+    Circle treasure = new Circle(tileSize / 2, Color.GOLD);
+    treasure.setCenterX(xOffset + (gameMap.getTreasure().getX() + 0.5) * tileSize);
+    treasure.setCenterY(yOffset + (gameMap.getTreasure().getY() + 0.5) * tileSize);
+    gamePane.getChildren().add(treasure);
+
+    // TODO : changer taille écran titre aussi
+    // TODO : changer police du titre
+    // TODO : mettre une taille max de la carte, selon résolution écran utilisateur
+    // TODO : new Game screen avec selection Nom aventurier et taille de la map
+    // TODO : ajouter logo jeu à gauche et dans barre des tâches
   }
 }
+
 
 
